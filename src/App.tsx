91c7afacbb495e0432a2084612426889c20f9697
Smirnov Stanislav <smistav@ya.ru>
2022-02-17 14:22:27 +0300

    work v.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
 import style from './App.module.scss'
-import { CSSTransition } from 'react-transition-group'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
+import { ModalFull } from './components/ModalFull'
 
 export const App = () => {
-  const [open, setOpen] = useState(false)
-  const modalRef = useRef<HTMLElement | null>(null)
+  const [open1, setOpen1] = useState(false)
+  const [open2, setOpen2] = useState(false)
+  const [open3, setOpen3] = useState(false)
+  const widthBegin = 500
+  const heightBegin = 200
+
+  const defaultModale = {
+    width: `${widthBegin}px`,
+    height: `${heightBegin}px`,
+  }
   return (
     <div className={style.component}>
-      <div className={style.modal}>
-        <div className={style.button}></div>
+      <div className={style.overlay} onClick={() => setOpen1(false)}>
+        <div
+          id='modal1'
+          style={{ ...defaultModale }}
+          className={style.modal}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className={style.button} onClick={() => setOpen1(!open1)}></div>
+          <ModalFull
+            widthBegin={widthBegin}
+            heightBegin={heightBegin}
+            open={open1}
+            setOpen={setOpen1}
+            element={'modal1'}
+          />
+        </div>
       </div>
-      <div className={style.modal}>
-        <div className={style.button} onClick={() => setOpen(!open)}></div>
-        <CSSTransition nodeRef={modalRef} in={open} timeout={500} classNames={{ ...style }}>
-          <div ref={modalRef as React.RefObject<HTMLDivElement>} className={style.modalFull}>
-            <div className={style.button} onClick={() => setOpen(!open)}></div>
-          </div>
-        </CSSTransition>
+      <div className={style.overlay} onClick={() => setOpen2(false)}>
+        <div
+          id='modal2'
+          style={{ ...defaultModale }}
+          className={style.modal}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className={style.button} onClick={() => setOpen2(!open2)}></div>
+          <ModalFull
+            widthBegin={widthBegin}
+            heightBegin={heightBegin}
+            open={open2}
+            setOpen={setOpen2}
+            element={'modal2'}
+          />
+        </div>
       </div>
-      <div className={style.modal}>
-        <div className={style.button}></div>
+      <div className={style.overlay} onClick={() => setOpen3(false)}>
+        <div
+          id='modal3'
+          style={{ ...defaultModale }}
+          className={style.modal}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className={style.button}></div>
+          <ModalFull
+            widthBegin={widthBegin}
+            heightBegin={heightBegin}
+            open={open3}
+            setOpen={setOpen3}
+            element={'modal3'}
+          />
+        </div>
       </div>
     </div>
   )
--- src/components/ModalFull.tsx
+++ src/components/ModalFull.tsx
@@ -1,4 +1,106 @@
-import style from './ModalFull.module.scss'
-export const ModalFull = () => {
-  return <div></div>
+import { useRef } from 'react'
+import { Transition } from 'react-transition-group'
+
+type ModalFullProps = {
+  open: boolean
+  setOpen: (arg: boolean) => void
+  element: string
+  widthBegin: number
+  heightBegin: number
+}
+
+export const ModalFull: React.FC<ModalFullProps> = ({
+  open,
+  setOpen,
+  element,
+  widthBegin,
+  heightBegin,
+}) => {
+  const widthEnd = 1400
+  const heightEnd = 800
+  const second = 500
+  const effect = 'ease-out'
+
+  const defaultStyle = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: `${widthBegin}px`,
+    height: `${heightBegin}px`,
+    borderRadius: '10px',
+    overflow: 'hidden',
+    backgroundColor: 'transparent',
+    boxSizing: 'border-box',
+    zIndex: 2,
+    padding: '20px',
+    border: '1px solid transparent',
+  }
+  const buttonStyle = {
+    position: 'absolute',
+    padding: '0',
+    margin: '0',
+    border: 'none',
+    backgroundColor: 'grey',
+    borderRadius: '5px',
+    width: '40px',
+    height: '40px',
+  }
+  const transition = {
+    transitionStyles: {
+      entering: {},
+      entered: {
+        top: 45,
+        left: 0,
+        width: `${widthEnd}px`,
+        height: `${heightEnd}px`,
+        padding: '20px',
+        backgroundColor: 'white',
+        border: '1px solid grey',
+        transition: `all ${second}ms ${effect}`,
+      },
+      exiting: {
+        top: 0,
+        left: 0,
+        width: `${widthBegin}px`,
+        height: `${heightBegin}px`,
+        padding: '20px',
+        backgroundColor: 'transparent',
+        transition: `all ${second}ms ${effect}`,
+      },
+      exited: {},
+    } as { [key: string]: React.CSSProperties },
+  }
+  const modalRef = useRef<HTMLElement | null>(null)
+  ;(function calculationShift() {
+    const xCenter = window.innerWidth / 2
+    const yCenter = window.innerHeight / 2
+    const elementBegin = document.querySelector(`#${element}`)
+    const y = elementBegin?.getBoundingClientRect()?.y
+    const x = elementBegin?.getBoundingClientRect()?.x
+    y && (transition.transitionStyles.entered.top = yCenter - y - heightEnd / 2)
+    x && (transition.transitionStyles.entered.left = xCenter - x - widthEnd / 2)
+  })()
+
+  return (
+    <>
+      <Transition nodeRef={modalRef} in={open} timeout={{ enter: 0, exit: second }}>
+        {(state) => (
+          <div
+            ref={modalRef as React.RefObject<HTMLDivElement>}
+            style={{
+              ...(defaultStyle as React.CSSProperties),
+              ...transition.transitionStyles[state],
+            }}
+          >
+            <div
+              style={{ ...(buttonStyle as React.CSSProperties) }}
+              onClick={() => setOpen(!open)}
+            ></div>
+          </div>
+        )}
+      </Transition>
+    </>
+  )
 }
